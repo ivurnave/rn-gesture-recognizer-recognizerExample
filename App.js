1eefb5136e1dc647ce63d9ca45c43aca7802d9f5
Evan Holmes <linkoo22@gmail.com>
2018-11-05 09:59:23 -0600

    working version of the recognition

--- App.js
+++ App.js
@@ -1,24 +1,51 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
-import {Trainer} from 'rn-gesture-recognizer';
+import {Trainer, Recognizer} from 'rn-gesture-recognizer';
+import TrainerApp from './Trainer';
+// import RecognizerApp from './Recognizer';
 
-export default class App extends Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text>Testing the rn-gesture-recognizer Trainer Component!</Text>
-        <Trainer
-          color={'#ff0000'}
-          strokeWidth={4}
-          path={'/Users/evan/Documents/capstone/trainerTest'}
-        />
-      </SafeAreaView>
-    );
-  }
-}
+let json = require('./gestureClasses.json');
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <SafeAreaView style={styles.container}>
+//         <Text>Testing the rn-gesture-recognizer Trainer Component!</Text>
+//         <Trainer
+//           color={'#ff0000'}
+//           strokeWidth={4}
+//           path={'/Users/evan/Documents/capstone/trainerTest'}
+//         />
+//       </SafeAreaView>
+//       // <TrainerApp />
+//       // <RecognizerApp />
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   }
 });
+
+export default class RecognizerApp extends Component {
+  render() {
+    console.log(json);
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Draw gestures to be recognized below!</Text>
+        <Recognizer
+          color={'#ff0000'}
+          strokeWidth={4}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   }
+// });
