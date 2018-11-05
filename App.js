b7c81e26d8c87ed2799e88104dbd58966f0289a5
Evan Holmes <linkoo22@gmail.com>
2018-11-05 10:07:39 -0600

    cleaning

--- App.js
+++ App.js
@@ -1,33 +1,9 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
-import {Trainer, Recognizer} from 'rn-gesture-recognizer';
-import TrainerApp from './Trainer';
-// import RecognizerApp from './Recognizer';
+import {Recognizer} from 'rn-gesture-recognizer';
 
 let json = require('./gestureClasses.json');
 
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <SafeAreaView style={styles.container}>
-//         <Text>Testing the rn-gesture-recognizer Trainer Component!</Text>
-//         <Trainer
-//           color={'#ff0000'}
-//           strokeWidth={4}
-//           path={'/Users/evan/Documents/capstone/trainerTest'}
-//         />
-//       </SafeAreaView>
-//       // <TrainerApp />
-//       // <RecognizerApp />
-//     );
-//   }
-// }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-});
 
 export default class RecognizerApp extends Component {
   render() {
@@ -44,8 +20,8 @@ export default class RecognizerApp extends Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   }
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
