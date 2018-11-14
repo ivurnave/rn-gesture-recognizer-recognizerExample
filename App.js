39666118b6f76b23d80e99d1183fd5deb3df1a95
Evan Holmes <linkoo22@gmail.com>
2018-11-14 07:46:47 -0600

    Adding the gesture recognition callback

--- App.js
+++ App.js
@@ -2,19 +2,33 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
 import {Recognizer} from 'rn-gesture-recognizer';
 
-let json = require('./gestureClasses.json');
-
+let gestureClasses = require('./gestureClasses.json');
 
 export default class RecognizerApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gesture: ''
+    }
+  }
+
+  myCallback = (dataFromChild) => {
+    this.setState({
+      gesture: dataFromChild
+    });
+  }
+
   render() {
-    console.log(json);
     return (
       <SafeAreaView style={styles.container}>
-        <Text>Draw gestures to be recognized below!</Text>
+        <Text style={styles.text}>Draw gestures to be recognized below!</Text>
         <Recognizer
           color={'#ff0000'}
           strokeWidth={4}
+          trainingData={gestureClasses}
+          recognitionHandler={this.myCallback}
         />
+        <Text style={styles.text}>{this.state.gesture}</Text>
       </SafeAreaView>
     );
   }
@@ -23,5 +37,8 @@ export default class RecognizerApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  text: {
+    textAlign: 'center'
   }
 });
